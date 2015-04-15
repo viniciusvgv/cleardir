605d3fbffe337280c00f85e285d4148ea3757beb
Vinícius Veroneze <vinicius.veroneze@tagview.com.br>
2015-04-15 14:48:12 -0300

    improving the log and making the module independent

--- cleardir.js
+++ cleardir.js
@@ -1,31 +1,57 @@
+// use strict
+
 var fs = require("fs")
 
-var ClearDir = function(path){
+var ClearDir = function(path) {
   this.path = path;
   this.unwantedFiles = [/\.class$/, /\.DS_STORE$/, /\.rspec$/];
 }
 
-ClearDir.prototype.clear = function(){
+ClearDir.prototype.clear = function(callback) {
   var found = [];
   var self = this;
 
   fs.readdir(this.path, function(err, files) {
-    if (err) return [];
+    if (err) {
+      callback(err);
+    }
 
-    files.forEach(function(file) {
+    files = files.filter(function(file){
+      var matches = false;
+
       self.unwantedFiles.forEach(function(exp) {
         if (file.match(exp)) {
-          fs.unlink(file, function (err) {
-            if (err){
-              throw err;
-            } else {
-              console.log("Deleted " + file);
-            }
-          });
+          matches = true;
+          return;
         }
       });
+
+      return matches;
     });
+
+    if (files.length < 1) {
+      return callback(null, []);
+    } else {
+      var pending = files.length, deleted = [];
+
+      files.forEach(function(file) {
+        fs.unlink(file, function(err){
+          if (err) {
+            callback(err);
+          } else {
+            deleted.push(file);
+          }
+
+          pending--;
+
+          if (pending == 0){
+            callback(null, deleted);
+          }
+        });
+      });
+    }
+
   });
-}
+};
 
 module.exports = ClearDir;
--- cli.js
+++ cli.js
@@ -4,7 +4,20 @@ var ClearDir = require('./cleardir');
 var cwd = process.cwd();
 var cleardir = new ClearDir(cwd);
 
-console.log("Cleaning '" + cwd + "'...")
+console.log("Cleaning '" + cwd + "'...");
 
-cleardir.clear();
+cleardir.clear(function(err, deleted) {
+  console.log("callingback with "+err+" and deleted "+deleted);
+  if (err) {
+    console.log("Failed to clean directory");
+  } else {
+    if (deleted.length > 0) {
+      deleted.forEach(function(file) {
+        console.log(file + " was deleted");
+      });
+    } else {
+      console.log("Nothing to delete");
+    }
+  }
+});
 
